0ac0d6a21a50108d94b1478a4b390e5929b3fed7
Carly Jackson <carlysjackson2@gmail.com>
2021-04-09 09:00:24 +1000

    URL request.

--- Assets/Script/search-results.js
+++ Assets/Script/search-results.js
@@ -3,7 +3,8 @@ var searchFormEl = document.querySelector("#search-form");
 var searchButton = document.querySelector("#search-input-button");
 var searchInputEl = document.querySelector("#search-input-area");
 var searchItemEl = document.querySelector("#past-searches-container");
-
+var cityTitleEl = document.getElementById("city-searched-item")
+var APIKEY = '510c27e4545e6077957004db2b092e1f';
 
 // fetch APIUrl
 
@@ -28,29 +29,96 @@ function getParams() {
 
 
 // URL
-function getApi() {
-    var requestUrl = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid=510c27e4545e6077957004db2b092e1f';
+function getWeatherFromCity(citySearched) {
+    var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=';
+    var queryUrl = requestUrl + citySearched + '&appid=' + APIKEY;
 
+    // If request isn't accepted
     fetch(requestUrl)
         .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function (response) {
+
             return response.json();
         })
 
-}
+        .then(function (locRes) {
+            // write query to page so
+            cityTitleEl.textContent = locRes.search.citySearched;
+
+            // Searched city not found
+            if (!locRes.results.length) {
+                console.log('No results found!');
+                var resultBody = document.createElement('div');
+                resultBody.classList.add('alert-red');
+                resultCard.append(resultBody);
+                resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+            } else {
+                resultContentEl.textContent = '';
+                for (var i = 0; i < locRes.results.length; i++) {
+                    printResults(locRes.results[i]);
+                }
+            }
+        }
+
+            // Get long and lat
+            // Longatude
+            var longatudeQuery = Data.coord.long
+    // Latitude
+    titleEl.textContent = resultObj.name;
 
 
-// if weather is nice/high temperature - present a sun
+    function getUvRating()
 
-// if weather is cold/low temperature/wet - present a cloud
 
-// FIVE DAY WEATHER FORECAST 
-// input
+    // Print results
+    function printResults(resultObj) {
+        console.log(resultObj);
 
-// depending on weather, insert:
-// rain cloud
+        // set up `<div>` to hold result content
+        var resultCard = document.createElement('div');
+        resultCard.classList.add('card', 'col-6', 'text-dark', 'mb-3', 'p-3');
 
-// sun
+        var resultBody = document.createElement('div');
+        resultBody.classList.add('card-body');
+        resultCard.append(resultBody);
 
-// cloud
+        // City Title
+        var titleEl = document.createElement('h3');
+        titleEl.textContent = resultObj.name;
 
-// partly sunny/partly cloudy
+        // Date Temperature
+        var bodyContentEl = document.createElement('p');
+        bodyContentEl.innerHTML = // Temperature
+            '<strong>Temp:</strong> ' + resultObj.temp + '<br/>';
+
+        if (resultObj.subject) { // Wind
+            bodyContentEl.innerHTML +=
+                '<strong>Wind:</strong> ' + resultObj.wind + '<br/>';
+        } if (resultObj.description) { // Humidity
+            bodyContentEl.innerHTML +=
+                '<strong>Humidity:</strong> ' + resultObj.humidity[0];
+        } if (resultObj.description) { // UV Index
+            bodyContentEl.innerHTML +=
+                '<strong>Description:</strong> ' + resultObj.description[0];
+        } else {
+            bodyContentEl.innerHTML +=
+                '<strong>Description:</strong>  No description for this entry.';
+        }
+
+        var linkButtonEl = document.createElement('a');
+        linkButtonEl.textContent = 'Read More';
+        linkButtonEl.setAttribute('href', resultObj.url);
+        linkButtonEl.classList.add('btn', 'btn-dark');
+
+        resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+
+        resultContentEl.append(resultCard);
+    }
+
+
+    getParams();
