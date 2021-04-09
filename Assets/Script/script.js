213aaae72318106f2a0cda884a7cb82e7595a45a
Carly Jackson <carlysjackson2@gmail.com>
2021-04-09 14:57:45 +1000

    API fetched correctly, first API data recorded on screen.

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,11 +1,15 @@
 var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector("#search-form");
+var searchFormEl = document.getElementById("#search-form");
 var searchButton = document.querySelector("#search-input-button");
 var searchInputEl = document.querySelector("#search-input-area");
 var searchItemEl = document.querySelector("#past-searches-container");
+var APIKEY = '510c27e4545e6077957004db2b092e1f';
+
+
+console.log(searchFormEl);
 
 // Search Form Input
-var formSubmitHandler = function (event) {
+function formSubmitHandler(event) {
     event.preventDefault();
 
     var citySearched = searchInputEl.value.trim();
@@ -17,16 +21,111 @@ var formSubmitHandler = function (event) {
         return;
     }
 
-    var queryString = './search-results.html?q=' + citySearched + '&units=metric';
-    // location.assign(queryString);
-
     // write query to page so user knows what they are viewing
     var listEl = document.createElement("LI");
     var previousList =
         document.createTextNode(citySearched);
     listEl.appendChild(previousList);
     document.getElementById("past-searches-container").appendChild(listEl);
+
+    getWeatherFromCity(citySearched);
+    getUvIndex();
 }
 
-searchFormEl.addEventListener('submit', formSubmitHandler);
+function getWeatherFromCity(citySearched) {
+    console.log("Inside the getWeatherFromCity function");
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    var queryUrl = requestUrl + citySearched + '&appid=' + APIKEY + '&units=metric';
+    console.log(queryUrl);
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+
+            printResults(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+    // // Searched city not found
+    // if (!locRes.results.length) {
+    //     console.log('No results found!');
+    //     var alertBody = document.createElement('div');
+    //     alertBody.classList.add('alert alert-info');
+    //     resultCard.append(alertBody);
+    //     resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+    // }
+}
+
+// UV Index API
+function getUvIndex() {
+    // Get longatude and latitude
+    var longatudeQuery = resultObj.coord.lon;
+    var latitudeQuery = resultObj.coord.lat;
+
+    console.log("Inside the getUvIndex function");
+
+    var requestUrl = 'api.openweathermap.org/data/2.5/forecast/daily?';
+    var queryUrl = requestUrl + 'lat=' + latitudeQuery + '&lon=' + longatudeQuery + '&appid=' + APIKEY;
+
+    console.log(queryUrl);
+
+    fetch(queryUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+
+            printResults(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+function printResults(resultObj) {
+    console.log(resultObj);
+
+    // set up `<div>` to hold result content
+    var resultCard = document.createElement('div');
+    resultCard.classList.add('card');
+
+    var resultBody = document.createElement('div');
+    resultBody.classList.add('card-body');
+    resultCard.append(resultBody);
+
+    // City Title
+    var titleEl = document.createElement('h3');
+    console.log(resultObj.name);
+    titleEl.textContent = resultObj.name;
+    resultBody.append(titleEl);
+
+    // Temperature
+    var bodyContentOneEl = document.createElement('p');
+    bodyContentOneEl.innerHTML =
+        '<strong>Temp:</strong> ' + resultObj.main.temp + '<br/>';
+    resultBody.append(bodyContentOneEl);
+    // Wind
+    var bodyContentTwoEl = document.createElement('p');
+    bodyContentTwoEl.innerHTML =
+        '<strong>Wind:</strong> ' + resultObj.wind.speed + '<br/>';
+    resultBody.append(bodyContentTwoEl);
+    // Humidity
+    var bodyContentThreeEl = document.createElement('p');
+    bodyContentThreeEl.innerHTML =
+        '<strong>Humidity:</strong> ' + resultObj.main.humidity + '<br/>';
+    resultBody.append(bodyContentThreeEl);
+
+    // // UV Index
+    // var bodyContentFourEl = document.createElement('p');
+    // bodyContentThreeEl.innerHTML =
+    //     '<strong>UV Index:</strong> ' + resultObj.main.humidity + '<br/>';
+    // resultBody.append(bodyContentThreeEl);
+
+    resultContentEl.append(resultCard);
+}
+
+searchButton.addEventListener('click', formSubmitHandler);
 
--- Assets/Script/search-results.js
+++ Assets/Script/search-results.js
@@ -1,124 +1,25 @@
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector("#search-form");
-var searchButton = document.querySelector("#search-input-button");
-var searchInputEl = document.querySelector("#search-input-area");
-var searchItemEl = document.querySelector("#past-searches-container");
-var cityTitleEl = document.getElementById("city-searched-item")
-var APIKEY = '510c27e4545e6077957004db2b092e1f';
 
-// fetch APIUrl
+// function getParams() {
+//     // URL search params and convert it to an array (i.e. ['?q=london', 'format=photo'])
+//     var searchParamsArr = document.location.search.split('&');
 
+//     console.log(searchParamsArr);
 
+//     // Get the query and format values
+//     var query = searchParamsArr[0].split('=').pop();
+//     var format = searchParamsArr[1].split('=').pop();
 
-// Split URL
-function getParams() {
-    // URL search params and convert it to an array (i.e. ['?q=london', 'format=photo'])
-    var searchParamsArr = document.location.search.split('&');
-
-    // Get the query and format values
-    var query = searchParamsArr[0].split('=').pop();
-    var format = searchParamsArr[1].split('=').pop();
-
-    searchApi(query, format);
-}
+//     searchApi(query, format);
+// }
 
 
 
-
-// RESULT CONTENT DIV
-
-
-// URL
-function getWeatherFromCity(citySearched) {
-    var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=';
-    var queryUrl = requestUrl + citySearched + '&appid=' + APIKEY;
-
-    // If request isn't accepted
-    fetch(requestUrl)
-        .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
-            return response.json();
-        })
-        .then(function (response) {
-
-            return response.json();
-        })
-
-        .then(function (locRes) {
-            // write query to page so
-            cityTitleEl.textContent = locRes.search.citySearched;
-
-            // Searched city not found
-            if (!locRes.results.length) {
-                console.log('No results found!');
-                var resultBody = document.createElement('div');
-                resultBody.classList.add('alert-red');
-                resultCard.append(resultBody);
-                resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-            } else {
-                resultContentEl.textContent = '';
-                for (var i = 0; i < locRes.results.length; i++) {
-                    printResults(locRes.results[i]);
-                }
-            }
-        }
-
-            // Get long and lat
-            // Longatude
-            var longatudeQuery = Data.coord.long
-    // Latitude
-    titleEl.textContent = resultObj.name;
-
-
-    function getUvRating()
-
-
-    // Print results
-    function printResults(resultObj) {
-        console.log(resultObj);
-
-        // set up `<div>` to hold result content
-        var resultCard = document.createElement('div');
-        resultCard.classList.add('card', 'col-6', 'text-dark', 'mb-3', 'p-3');
-
-        var resultBody = document.createElement('div');
-        resultBody.classList.add('card-body');
-        resultCard.append(resultBody);
-
-        // City Title
-        var titleEl = document.createElement('h3');
-        titleEl.textContent = resultObj.name;
-
-        // Date Temperature
-        var bodyContentEl = document.createElement('p');
-        bodyContentEl.innerHTML = // Temperature
-            '<strong>Temp:</strong> ' + resultObj.temp + '<br/>';
-
-        if (resultObj.subject) { // Wind
-            bodyContentEl.innerHTML +=
-                '<strong>Wind:</strong> ' + resultObj.wind + '<br/>';
-        } if (resultObj.description) { // Humidity
-            bodyContentEl.innerHTML +=
-                '<strong>Humidity:</strong> ' + resultObj.humidity[0];
-        } if (resultObj.description) { // UV Index
-            bodyContentEl.innerHTML +=
-                '<strong>Description:</strong> ' + resultObj.description[0];
-        } else {
-            bodyContentEl.innerHTML +=
-                '<strong>Description:</strong>  No description for this entry.';
-        }
-
-        var linkButtonEl = document.createElement('a');
-        linkButtonEl.textContent = 'Read More';
-        linkButtonEl.setAttribute('href', resultObj.url);
-        linkButtonEl.classList.add('btn', 'btn-dark');
-
-        resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-        resultContentEl.append(resultCard);
-    }
-
-
-    getParams();
+//     // Searched city not found
+//     //     // if (!locRes.results.length) {
+//     //     //     console.log('No results found!');
+//     //     //     var resultBody = document.createElement('div');
+//     //     //     resultBody.classList.add('alert alert-info');
+//     //     //     resultCard.append(resultBody);
+//     //     //     resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+//     //     }
+// }
